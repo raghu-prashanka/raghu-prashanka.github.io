2138d2822dccc7bef1a21d516e9262693a8c8895
Raghu Prashanka <braghuprashanka@gmail.com>
2024-11-20 17:44:17 +0530

    udt

---
+++ temp.js
@@ -0,0 +1,16 @@
+db.products.insertMany(
+    [
+        {
+            "name": "Product 7",
+            "description":"This is the description of product 7",
+            "price": 150,
+            "url":"https://picsum.photos/id/1/300/300"
+        },
+        {
+            "name": "Product 8",
+            "description":"This is the description of product 8",
+            "price": 250,
+            "url":"https://picsum.photos/id/1/300/300"
+        }
+    ]
+);
